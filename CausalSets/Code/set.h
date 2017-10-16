fc503218f568ba034c521063b4d5d02a1d22db44
flurrie <fleurversteegen@gmail.com>
2017-10-16 09:23:13 +0200

    new file:   data2.dat 	new file:   elements.h 	new file:   main.cpp 	new file:   neighbours.dat 	new file:   old_set.cpp 	new file:   set.cpp 	new file:   set.h 	new file:   spacetime.cpp 	new file:   spacetime.h

---
+++ CausalSets/Code/set.h
@@ -0,0 +1,44 @@
+#ifndef SET_H
+#define SET_H
+# include "spacetime.h" 
+# include "elements.h"
+# include <vector>
+using namespace std;
+
+class Set 
+{
+private:    
+       int * nrElem;         //pointer to the nr of elements present in the set
+       Element *root, *tail;
+      
+    
+    int *near_neigh;       //points to the id's of the nearest neighbours of a set
+    Spacetime st;         //spacetime the set is build from
+  
+
+vector <int> C;          //Causal matrix represented as 1dim array
+
+vector <int> L;          //Link matrix represented as 1dim array
+
+public:
+                //array that holds the elements in the set
+    Set(Spacetime);     //construct a set based on a spacetime with a fixed nr of elements --> this is where the sprinkling should go
+
+    ~Set();
+
+    int *GetNrElem();           //get the nr of elements present in the set
+
+    void AddElem(Element& );    // add a new element to the set
+    
+    void NN(Element&, ofstream& ); // finds the nearest neighbours of an element
+    
+    Element *GetRoot();
+  //  static void display(Element *);
+    
+    vector<int> M_Link();
+   // vector<int> M_Link_2();
+    vector<int>  M_Causal(ofstream&);
+    void display(vector<int>& );
+    
+};
+#endif
