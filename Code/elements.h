921ce9d6fb5e3a33b6a93434dd5eddad5cdf9efa
fleurversteegen <fleurversteegen@gmail.com>
2017-10-25 15:05:30 +0200

    Add files via upload

---
+++ Code/elements.h
@@ -0,0 +1,71 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+# include <vector>
+
+using namespace std;
+
+
+
+struct Element
+{   
+
+    double * pos;             // holds the position of the element in the spacetime
+    vector <int> near_neigh;  // holds a vector of 
+    int  nr_of_nn;
+    int id,d;
+    Element *next, *prev;
+    
+
+//CONSTRUCTOR
+//Constructs a element and sets it's default values to 0
+    Element():id(0),pos(0),d(0), next(NULL),prev(NULL), nr_of_nn(0)
+    {         
+          
+    }
+
+ //CONSTRUCTOR
+ //Constructs an element give an id, position and spacetime dimension
+
+    Element(int _id, double *_pos, int _d) : id(_id), d(_d), next(NULL), prev(NULL), nr_of_nn(0)    
+    {   
+        pos = new double[d];
+        for(int i = 0; i<d; i++)
+        {
+            pos[i] = _pos[i];
+            
+        }                  
+        
+    }
+//COPY CONSTRUCTOR
+//Copies an element 
+    
+    Element (Element *e)
+    {
+        Element(e->id, e->pos, e->d );
+      //  next = NULL;
+    }
+
+//function to check if Element with index n is already a neighbour
+    bool is_nn(int n)
+    {
+        for(int i = 0; i < near_neigh.size(); i++)
+        {
+            if(near_neigh[i] == n)
+            {   
+                return true;
+            }
+        }
+        return false;
+    }
+
+//DESTRUCTOR    
+    ~Element()
+    {
+       delete [] pos;    
+    }
+    
+
+
+
+};
+#endif
---
+++ Code/set.cpp
@@ -0,0 +1,315 @@
+# include <iostream>
+# include <cstdlib>
+# include <fstream>
+# include "set.h"
+
+
+using namespace std;
+
+//CONSTRUCTOR
+//Create a set based on the spacetime _st and with nr of elements _nrElem
+
+Set :: Set(Spacetime _st) : st(_st)
+{
+    
+    root = NULL;               //declare a root ("dummy") element and a tail element of the singly list of elements 
+    tail = NULL;
+    nrElem = new long int;         //allocate space for the pointer that holds the nr of elements
+    *nrElem = 0;                   //initialize the nr of elemnts to 0
+}
+
+
+//DESTRUCTOR
+Set :: ~Set()                      
+{
+    Element *tmp = root;
+    while(tmp) 
+    {
+        root = tmp->next;
+        delete tmp;              //delete the next node in the linked list of elements
+        tmp = root;
+    }
+}
+
+
+
+
+//Adds element to Set
+void Set :: AddElem(Element &e)
+{ 
+  
+   Element *tmp =new Element(e.id, e.pos, e.d);       //create a tmp element from the new element
+  
+    if (root == NULL)                                 //if list has not been created
+    {                                                 //set the beginning and end to point at the new element
+      root = tmp;
+      tail = tmp; 
+                        
+    }
+    
+    else                                             //set the pointer of the next element equal to the new element
+    {   
+        tail->next = tmp;    
+        tmp->prev = tail;    
+        tail = tail->next; 
+    }
+    
+    *nrElem+=1;
+    
+}
+
+
+//returns the nr of elements in the set
+long int * Set :: GetNrElem(){ return nrElem; }
+
+
+//returns the pointer to the root element of the set
+Element * Set :: GetRoot(){ return root; } 
+//returns the pointer to the tail element of the set
+Element * Set :: GetTail(){ return tail; } 
+//returns the pointer to the nth element of the set
+Element * Set :: GetNode(int n)
+{  
+    Element * walker;
+    
+    if (n > (*nrElem)/2)
+    {   
+        walker = tail;
+        for (int i = *nrElem; i > (*nrElem -((*walker).id-1)); i--)
+        {
+            walker = walker->prev;
+        }
+    }
+    
+    else
+    {
+        walker = root;
+        for (int i = 1; i <n; i++)
+        {
+            walker = walker->next;
+        }
+    }
+    return walker;
+}
+
+//sets the ID's of the nearest neighbours of an element e
+void Set :: NN(Element &e, ofstream &Output)
+{
+   double *pos = e.pos;                                           //points to the position of e  
+   double * pos_n;
+   Element * walker = tail;                                       //walker points at the first element after the root element
+   double ds;
+   
+ 
+    while (walker != NULL)
+    {    pos_n = walker->pos;                                    //pos_n will hold the position of a second element e_n which is a possible nearest neighbour. 
+          
+        if ((*walker).id != e.id )                               //continue if the possible neighbour is not e itself
+        { 
+           
+            ds = st.MetDist(pos, pos_n);                         //calculate the metric distance between the two points
+          
+            if(ds>=0)                                              //if lightlike/timelike separated
+            {	
+                
+                e.is_nn(walker->id);
+                (e.near_neigh).push_back(walker->id);              //add a new ID to the array of nearest neighbours
+                e.nr_of_nn += 1;                                   // increase the nr of neighbours by 1
+            
+            }
+        }
+        walker = walker->prev;                                    //go to the next element in the set
+    } 
+}
+
+
+
+/*
+//sets the ID's of the nearest neighbours of an element e
+void Set :: NN(Element &e, ofstream &Output)
+{
+   double *pos = e.pos;                                           //points to the position of e  
+   double * pos_n;
+   Element * walker = root;                                       //walker points at the first element after the root element
+   double ds;
+   try
+   {
+       (e.near_neigh).reserve(*nrElem);
+   }
+   catch(const std::bad_alloc& ex)
+   {
+     cout<<"ERROR: NO MORE RAM!"<<endl;
+     exit(0);
+   }
+   
+    while (walker != NULL)
+    {    pos_n = walker->pos;                            //pos_n will hold the position of a second element e_n which is a possible nearest neighbour. 
+       
+        if ((*walker).id != e.id )                        //continue if the possible neighbour is not e itself
+        { 
+            ds = st.MetDist(pos, pos_n);                      //calculate the metric distance between the two points
+          
+            if(ds>=0)                                              //if lightlike/timelike separated
+            {	  
+                (e.near_neigh).push_back(walker->id);              //add a new ID to the array of nearest neighbours
+                e.nr_of_nn += 1;                                   // increase the nr of neighbours by 1
+               
+            }
+        }
+        walker = walker->next;                                    //go to the next element in the set
+    } 
+}
+
+
+//sets the ID's of the nearest neighbours of an element e
+void Set :: NN(Element &e, ofstream &Output)
+{
+   double *pos = e.pos;                                           //points to the position of e  
+   double * pos_n;
+   Element * walker = root;                                       //walker points at the first element after the root element
+   double ds;
+   
+   for (int i = 0; i<e.id-1; i++)
+   {
+       walker = walker->next;                                    //Only interested in the neighbours that lie in the future
+   }
+  
+ 
+    while (walker != NULL)
+    {    pos_n = walker->pos;                                    //pos_n will hold the position of a second element e_n which is a possible nearest neighbour. 
+          
+        if ((*walker).id != e.id )                               //continue if the possible neighbour is not e itself
+        { 
+           
+            ds = st.MetDist(pos, pos_n);                         //calculate the metric distance between the two points
+          
+            if(ds>=0)                                              //if lightlike/timelike separated
+            {	
+                (e.near_neigh).push_back(walker->id);              //add a new ID to the array of nearest neighbours
+                e.nr_of_nn += 1;                                   // increase the nr of neighbours by 1
+             cout<<walker->id<<" is a nn of: "<<e.id<<endl;
+            }
+        }
+        walker = walker->next;                                    //go to the next element in the set
+    } 
+}
+
+*/
+
+//Causal matrix
+
+vector<int>  Set :: M_Causal(ofstream &Output)
+{  
+   int size =((*nrElem - 1)*(*nrElem - 1) + (*nrElem - 1))/2; 
+  
+   C.resize(size); 
+  
+   Element * walker = root;                            
+   int row = 0;
+   int column;
+   int counter = *nrElem;
+  
+    while (walker != NULL)                                                    //loop over the elements in the set
+    {
+        NN(*walker, Output);    
+       
+        for (int i = 0; i < (*walker).nr_of_nn; i++)                                      //matrix is represented as a 1 dim array. Only the bottom half is stored
+        {
+            column = ((*walker).near_neigh[i]-1) - (*nrElem - counter +1);                
+            
+            if( (*walker).near_neigh[i] > (*walker).id )                              //if the element is in the causal future               
+            {  
+                C[column + row ]=1;                                                  //for each nearest neighbour set the corresponding entry to 1
+            }
+        } 
+
+        walker = walker->next;      
+        counter += -1;                                                       //Only store the right upper triangle of the matrix
+        row  += counter;
+    }
+     
+    return C; 
+}
+
+//Link Matrix
+vector<int> Set :: M_Link()
+{     
+    int size =((*nrElem - 1)*(*nrElem - 1) + (*nrElem - 1))/2; 
+    L.resize(size); 
+   
+    int Csq, max;                                           
+    int row=0;
+    int column;
+    int cnt = *nrElem - 1;
+
+ 
+        for (int i = 0; i < size; i++)                    //loop over elements in Causal matrix
+        {
+            Csq = 0;
+            max = row;
+            column = i - 1 + cnt;
+           
+            if(i == row)
+            {   
+                max++;
+                column += cnt - 1 - (max - row);
+            }
+            else
+            {
+                while(max < i)
+                {
+                    if(C[i]!=0)                             //if Element i is a neighbour
+                    {
+                        Csq += C[max] * C[column];          //calculate the square of the causal matrix
+                    }
+                    max++;
+                    column += cnt - 1 - (max - row);        //update column
+                }        
+                 
+            }  
+        
+            row += cnt*((column+1)/size);                   //update row
+            cnt += -((column+1)/size);
+           
+            if (Csq > 0)                                 //if the square of the causal matrix > 0, then the neighbour is not a nearest neighbour
+            {     
+                 L[i] = 0;//C[i] - 1;
+            }
+            else                                        //the neighbour is a nearest neighbour
+            {
+                L[i] = C[i];
+            }
+      }
+     
+    return L;
+}
+
+
+
+
+//display the link and causal matrix
+void Set :: display(vector <int> &Matrix)
+{  
+
+    int counter = 0;
+    for(int j = 0; j< (*nrElem); j++)
+    { 
+        for(int i = j*(*nrElem); i < j*(*nrElem)+*nrElem; i++)
+        { 
+            if( i - j*( 1 + *nrElem ) <= 0)                     //print zeros for bottom half of matrix
+                {cout<<0<<" ";}
+            else
+                {
+                    cout<<Matrix[counter]<<" "; 
+                    counter+=1;
+                }
+    
+        }
+        cout<<endl;
+    }
+
+}
+
+
+
+
